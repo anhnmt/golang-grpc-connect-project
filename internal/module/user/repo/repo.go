99c6ecb4e5b7f9b920b8effc5768df01dd53ca8f
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2022-09-09 11:49:10 +0700

    update user service

--- internal/module/user/repo/repo.go
+++ internal/module/user/repo/repo.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	usermodel "github.com/xdorro/golang-grpc-base-project/internal/module/user/model"
 	"github.com/xdorro/golang-grpc-base-project/pkg/repo"
@@ -13,6 +14,12 @@ var _ IRepo = (*Repo)(nil)
 
 // IRepo is the interface that must be implemented by a repository.
 type IRepo interface {
+	CountDocuments(filter any) (int64, error)
+	Find(filter any, opt ...*options.FindOptions) ([]*usermodel.User, error)
+	FindOne(filter any, opt ...*options.FindOneOptions) (*usermodel.User, error)
+	InsertOne(data any, opt ...*options.InsertOneOptions) (*mongo.InsertOneResult, error)
+	UpdateOne(filter, data any, opt ...*options.UpdateOptions) (*mongo.UpdateResult, error)
+	SoftDeleteOne(filter any, opt ...*options.UpdateOptions) (*mongo.UpdateResult, error)
 }
 
 // Repo is a repository struct.
@@ -33,4 +40,34 @@ func NewRepo(opt *Option) IRepo {
 	}
 
 	return r
+}
+
+func (r *Repo) CountDocuments(filter any) (int64, error) {
+	return repo.CountDocuments(r.collection, filter)
+}
+
+func (r *Repo) Find(filter any, opt ...*options.FindOptions) ([]*usermodel.User, error) {
+	return repo.Find[usermodel.User](r.collection, filter, opt...)
+}
+
+func (r *Repo) FindOne(filter any, opt ...*options.FindOneOptions) (*usermodel.User, error) {
+	return repo.FindOne[usermodel.User](r.collection, filter, opt...)
+}
+
+func (r *Repo) InsertOne(data any, opt ...*options.InsertOneOptions) (
+	*mongo.InsertOneResult, error,
+) {
+	return repo.InsertOne(r.collection, data, opt...)
+}
+
+func (r *Repo) UpdateOne(filter, data any, opt ...*options.UpdateOptions) (
+	*mongo.UpdateResult, error,
+) {
+	return repo.UpdateOne(r.collection, filter, data, opt...)
+}
+
+func (r *Repo) SoftDeleteOne(filter any, opt ...*options.UpdateOptions) (
+	*mongo.UpdateResult, error,
+) {
+	return repo.SoftDeleteOne(r.collection, filter, opt...)
 }
